7d1bd4862bcd90317ae9a3de56b3d27a00eedfd6
kiayria <karina.maikenova@gmail.com>
2021-03-17 21:51:26 +0300

    mocked test

--- tests/backend/test_jobfunnel.py
+++ tests/backend/test_jobfunnel.py
@@ -1,9 +1,13 @@
+import os
+from unittest.mock import patch
+
 from jobfunnel.backend.jobfunnel import JobFunnel
 from jobfunnel.config import get_config_manager, build_config_dict, parse_cli
 
 
-def test_run():
-    args = parse_cli(['load', '-s', 'tests_settings.yaml'])
+@patch('jobfunnel.backend.jobfunnel.JobFunnel.run')
+def test_jobfunnel_run(mock_run):
+    args = parse_cli(['load', '-s', os.path.dirname(os.path.realpath(__file__)) + '/tests_settings.yaml'])
     cfg_dict = build_config_dict(args)
 
     # Build config manager
@@ -13,4 +17,5 @@ def test_run():
     # Init
     job_funnel = JobFunnel(funnel_cfg)
     job_funnel.run()
-    assert True
+    assert job_funnel.run == mock_run
+    assert mock_run.called
